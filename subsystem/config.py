199bf130c276d18f63be4c5c5a41fec8a94fb1e9
Gehan Syafesi <gehansyafesi@gmail.com>
2018-04-10 19:18:40 +0800

    update database uri into heroku

--- subsystem/config.py
+++ subsystem/config.py
@@ -1,8 +1,8 @@
 # Postgres Database Data
-HOSTNAME = ""
-PORT = ""
-DB_NAME = ""
-UID = ""
-PWD = ""
+HOSTNAME = "ec2-54-225-96-191.compute-1.amazonaws.com"
+PORT = "5432"
+DB_NAME = "deiklt2rl7r40u"
+UID = "rdpiwwtjtvprln"
+PWD = "cc7f8c7fdf7db67d31230750ba89680851e60b57fb6650525874554b15afb225"
 # App secret key (random)
-SECRET_KEY = ""
+SECRET_KEY = "asdasjkdhl"
